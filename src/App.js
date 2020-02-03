f19f94546c9b9e732a3205c5bc31800a56723c76
ldail <leondailofficial@gmail.com>
2020-02-03 11:19:39 -0800

    testing for mobile deployment

--- src/App.js
+++ src/App.js
@@ -21,47 +21,51 @@ import { sidebarSelector, dropdownSelector } from './redux/navigation/navigation
 import Spinner from './components/Spinner/Spinner';
 
 
-function App({setUser, addItems, sidebarHidden, dropdownHidden, checkUser, fetchCollectionsStart}) {
-
-  const ShopMain = lazy(() => import('./pages/ShopMain/ShopMain'));
-  const ShopCollectionsPage = lazy(() => import('./pages/ShopCollectionsPage/ShopCollectionsPage'));
-  const CollectionPage = lazy(() => import('./pages/CollectionPage/CollectionPage'));
-  const SignIn = lazy(() => import('./pages/SignIn/SignIn'));
-  const Checkout = lazy(() => import('./pages/Checkout/Checkout'));
-
-  useEffect(() => {
-    fetchCollectionsStart();
-    checkUser();
-  }, []);
-
-  return (
-    <div className="App">
-      <Sidebar />
-      <Dropdown />
-      <Header />
-      <Switch>
-        <Suspense fallback={Spinner}>
-          <Route exact path="/" component={ShopMain} />
-          <Route exact path="/shop" component={ShopCollectionsPage} />
-          <Route path="/shop/:id" component={CollectionPage} />
-          <Route path="/signin" component={SignIn} />
-          <Route path="/checkout" component={Checkout} />
-        </Suspense>
-      </Switch>
-      <Footer />
-    </div>
-  );
+export default function App() {
+  return ( <div>This is an applicatino working</div> )
 }
 
-const mapStateToProps = createStructuredSelector({
-  sidebarHidden: sidebarSelector,
-  dropdownHidden: dropdownSelector
-})
-const mapDispatchToProps = dispatch => ({
-  setUser: (user) => dispatch(signIn(user)),
-  addItems: (items) => dispatch(addItems(items)),
-  checkUser: () => dispatch(checkUser()),
-  fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
-})
+// function App({setUser, addItems, sidebarHidden, dropdownHidden, checkUser, fetchCollectionsStart}) {
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+//   const ShopMain = lazy(() => import('./pages/ShopMain/ShopMain'));
+//   const ShopCollectionsPage = lazy(() => import('./pages/ShopCollectionsPage/ShopCollectionsPage'));
+//   const CollectionPage = lazy(() => import('./pages/CollectionPage/CollectionPage'));
+//   const SignIn = lazy(() => import('./pages/SignIn/SignIn'));
+//   const Checkout = lazy(() => import('./pages/Checkout/Checkout'));
+
+//   useEffect(() => {
+//     fetchCollectionsStart();
+//     checkUser();
+//   }, []);
+
+//   return (
+//     <div className="App">
+//       <Sidebar />
+//       <Dropdown />
+//       <Header />
+//       <Switch>
+//         <Suspense fallback={Spinner}>
+//           <Route exact path="/" component={ShopMain} />
+//           <Route exact path="/shop" component={ShopCollectionsPage} />
+//           <Route path="/shop/:id" component={CollectionPage} />
+//           <Route path="/signin" component={SignIn} />
+//           <Route path="/checkout" component={Checkout} />
+//         </Suspense>
+//       </Switch>
+//       <Footer />
+//     </div>
+//   );
+// }
+
+// const mapStateToProps = createStructuredSelector({
+//   sidebarHidden: sidebarSelector,
+//   dropdownHidden: dropdownSelector
+// })
+// const mapDispatchToProps = dispatch => ({
+//   setUser: (user) => dispatch(signIn(user)),
+//   addItems: (items) => dispatch(addItems(items)),
+//   checkUser: () => dispatch(checkUser()),
+//   fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
+// })
+
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
